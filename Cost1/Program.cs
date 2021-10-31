16876d99e89c0e9fdaa103c9201be358a895c125
Kirill <91422323+k1dobu3@users.noreply.github.com>
2021-10-31 18:23:49 +0300

    Add files via upload

---
+++ Cost1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Cost1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стоимость товара:");
+            double c;
+            double r;
+            double x = Convert.ToInt32(Console.ReadLine());
+
+            if (x >= 500 && x <= 1000)
+            {
+                c = x * 0.97;
+                Console.WriteLine("Цена товара с учетом скидки: " + c);
+            }
+            else if (x > 1000);
+            {
+                r = x * 0.95;
+                Console.WriteLine("Цена товара с учетом скидки: " + r);
+            }
+
+        }
+    }
+}
